ba98de216cdc4c0f6b9b4cff69a3cf1302d84527
jorenver <jorenver@espol.edu.ec>
2014-01-26 18:48:47 -0500

    se crearon metodos sets y gets

--- Jugador.py
+++ Jugador.py
@@ -7,6 +7,7 @@ class Jugador:
 		self._posY=y
 		self._color=c
 		self._velocidad=v
+		self._radio=20
 	
 	def avanzar(self):
 		self._posX=self._posX+self._velocidad
@@ -15,9 +16,31 @@ class Jugador:
 		self._posX=self._posX-self._velocidad
 		
 	def acender(self):
-		self._posY=self._posY+self._velocidad
-	
-	def descender(self):
 		self._posY=self._posY-self._velocidad
 	
+	def descender(self):
+		self._posY=self._posY+self._velocidad
 	
+	def getPosX(self):
+		return self._posX
+	
+	def getPosY(self):
+		return self._posY
+		
+	def getColor(self):
+		return self._color
+	
+	def getRadio(self):
+		return self._radio
+	
+	def setPosX(self,x):
+		self._posX=x
+	
+	def setPosY(self,y):
+		self._posY=y
+		
+	def setColor(self,c):
+		self._color=c
+	
+	def setRadio(self,r):
+		self._radio=r
