c3a543f3683759b8d754c009fb2f6cc13dbf46f8
Rodrigo <rodfcast@espol.edu.ec>
2014-01-29 10:49:27 -0500

    Se ha agregado la funcionalidad de teletransportacion del jugador en el escenario Dos

--- EscenarioDos.py
+++ EscenarioDos.py
@@ -7,16 +7,16 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4 import QtCore
 
-class Acciones:
+class Accion:
 	pasadizo=0
 	caida=1
 	salto=2
+	teletransportacion=3
 
 class EstadoEscenario:	
-	pasadizoOn=1
 	pasadizoOff=0
-
-
+	pasadizoOn=1	
+	pisoTres=2
 
 class EscenarioDos(Escenario):
 	pisoUno=None
@@ -29,9 +29,11 @@ class EscenarioDos(Escenario):
 
 	def __init__(self,*args): #constructor		
 		Escenario.__init__(self,*args)
-		self.jugador=Jugador(0,Escenario.dimension_y-self.nivel_piso_y-50,Qt.blue,10)#posicion inicial del jugador
+		color=QColor(0,0,255)		
+		self.jugador=Jugador(0,Escenario.dimension_y-self.nivel_piso_y-50,color,10)#posicion inicial del jugador
+		self.jugador.setRadio(50)
 		self.setWindowTitle("Escenario Dos")
-		self.thread_pintarPasadizo=Hilo(self,Acciones.pasadizo)
+		self.thread_pintarPasadizo=Hilo(self,Accion.pasadizo)
 		self.estadoEscenario=EstadoEscenario.pasadizoOff
 
 	def paintEvent(self, event):
@@ -43,7 +45,7 @@ class EscenarioDos(Escenario):
 		paint.drawImage(center,fondoEscenario)
 		self.dibujarPisos(paint)
 		paint.setBrush(self.jugador.getColor())
-		paint.drawEllipse(self.jugador.getPosX(),self.jugador.getPosY(), 50, 50)
+		paint.drawEllipse(self.jugador.getPosX(),self.jugador.getPosY(),self.jugador.getRadio(),self.jugador.getRadio())
 		paint.setBrush(Qt.green)
 		paint.drawRect(self.tam,Escenario.dimension_y-self.nivel_piso_y,self.thread_pintarPasadizo.valor_X,10)#rectangulo que se mueve
 		paint.end()
@@ -64,21 +66,21 @@ class EscenarioDos(Escenario):
 
 	def keyPressEvent(self,e):
 		if e.key()==QtCore.Qt.Key_Right:
-			self.valor=self.valor+self.jugador.getVelocidad()
-			self.jugador.setPosX(self.valor) #actualiza la posicion x del jugador cada vez que se presione una tecla
+			self.jugador.avanzar()
 			self.repaint()# se vuelve a pintar el circulo con las nuevas coordenadas
-			if self.valor>self.tam-10:  # si sale del piso Uno la pelota se cae al piso dos
-				if self.estadoEscenario==EstadoEscenario.pasadizoOff :#si el pasadizo esta cerrado entonces la pelota debe caer
-					self.my_thread=Hilo(self,Acciones.caida)
+			if self.jugador.getPosX()>self.tam-10:
+				if self.estadoEscenario==EstadoEscenario.pasadizoOff:  
+					self.my_thread=Hilo(self,Accion.caida)
 					self.my_thread.start()
 
 		if e.key()==QtCore.Qt.Key_Left:
-			self.valor=self.valor-self.jugador.getVelocidad()
-			self.jugador.setPosX(self.valor)
-			self.repaint()
+			self.jugador.retroceder()
+			self.repaint()	
 
+		if e.key()==QtCore.Qt.Key_X:
+			self.hilo=Hilo(self,Accion.teletransportacion)
+			self.hilo.start()
 
-				
 	def  actualizar(self):
 		self.thread_pintarPasadizo.start()
 
@@ -91,14 +93,17 @@ class Hilo(threading.Thread):
 		self.accion=accion #define el tipo de accion que se ejecutara
 		self.valor_X=0
 		self.valor_Y=0
+		self.light=self.light=0
 
 	def run(self):
-		if self.accion==0:#pasadizo
+		if self.accion==Accion.pasadizo:#pasadizo
 			self.pintarPasadizo()
-		elif self.accion==1:
+		elif self.accion==Accion.caida:# jugador se cae
 			self.jugadorCaida()
-		elif self.accion==2:
+		elif self.accion==Accion.salto: #jugador salta
 			self.jugadorSaltar()
+		elif self.accion==Accion.teletransportacion: # teletransportacion
+			self.jugadorTeletransportacion()
 
 	def pintarPasadizo(self):
 		self.valor_X=0
@@ -119,12 +124,28 @@ class Hilo(threading.Thread):
 			self.escenarioDos.jugador.setPosX(self.valor_X)
 			self.escenarioDos.jugador.setPosY(self.valor_Y)
 			self.escenarioDos.repaint()
-			if self.valor_Y>self.escenarioDos.dimension_y-50-20:
+			if self.valor_Y>self.escenarioDos.dimension_y-70:
 				self.escenarioDos.actualizar()
 				break
 
 	def jugadorSaltar():
-		pass	
+		pass
+
+	def jugadorTeletransportacion(self):
+		while True:
+			self.light=self.light+30
+			color=QColor.light(self.escenarioDos.jugador.getColor(),self.light)
+			self.escenarioDos.jugador.setColor(color)#cambiando el color en cada iteraccion # si color es mayor que un valor x se cambia la posicion del jugador a posDestino_X,PosDestion_Y
+			self.escenarioDos.repaint()
+			time.sleep(0.25)
+			if(self.light>100):
+				self.escenarioDos.jugador.setPosX(self.escenarioDos.tam+self.escenarioDos.nivel_piso_x) 
+				self.escenarioDos.jugador.setPosY(self.escenarioDos.dimension_y-self.escenarioDos.nivel_piso_y-self.escenarioDos.jugador.getRadio())
+				self.escenarioDos.repaint()
+				self.escenarioDos.estadoEscenario=EstadoEscenario.pisoTres
+				break;
+		pass
+
 
 
 
